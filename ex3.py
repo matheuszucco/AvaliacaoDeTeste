d0abbb55b52a9583dc83430bff5c565ae352850a
Matheus Zucco <matheus62315746@alunos.sc.senac.br>
2026-06-15 21:37:30 -0300

    Exercicio 3

--- ex3.py
+++ ex3.py
@@ -1,7 +1,11 @@
-total = 0
-numero = 1
-while numero != 0:
-    numero = int(input("Informe um número inteiro: "))
-    total = total + numero
-
-print(f"A soma dos números digitados é {total}")
+# Peça ao usuário para inserir um número positivo N.
+# O programa deve exibir todos 
+# os números de 1 até N utilizando um loop while.​
+numero = int(input("Informe um número positivo"))
+while numero <=0:
+    print("Número Inválido")
+    numero = int(input("Informe um número positivo"))
+indice = 1
+while indice <= numero:
+    print(indice)
+    indice +=1
